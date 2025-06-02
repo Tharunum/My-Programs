8d29bf3d52ad34af7e02632f3b2d2b2522e82b75
Tharunum <128670983+Tharunum@users.noreply.github.com>
2025-06-02 16:33:38 +0530

    Odd.java
    updated

--- Odd.java
+++ Odd.java
@@ -1,8 +1,10 @@
-Class Oddnum{
-  Public static void main(String args[]){
-    int num={1,3,2,5,4,6};
-    if(num%2!=0){
-      System.out.orintln(num);
+class Oddnum{
+  public static void main(String args[]){
+    int num[]={1,3,2,5,4,6};
+    for(int i=0;i<num.length;i++){
+    if(num[i]%2!=0){
+      System.out.println(num[i]);
+    }
     }
   }
 }
