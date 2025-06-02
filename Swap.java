28ffc893647ca5ce3542c1772b3df3a1abda9ccb
Tharunum <128670983+Tharunum@users.noreply.github.com>
2025-06-02 16:46:03 +0530

    Create Swap.java
    added

---
+++ Swap.java
@@ -0,0 +1,10 @@
+class SwapNum{
+public static void main(String args[]){
+int temp,a=2,b=4;
+System.out.println("Before Swap:"+a+" "+b);
+  temp=a;
+  a=b;
+  b=temp;
+   System.out.println("After Swap:"+a+" "+b);
+}
+}
